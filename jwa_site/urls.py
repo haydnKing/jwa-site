6f7de37ccc367c9fde15890435b0d742ef340669
Haydn King <hjking734@gmail.com>
2014-11-20 17:59:29 +0000

    Basic homepage gets served but doesn't work

--- jwa_site/urls.py
+++ jwa_site/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+
+from labdata.admin import admin_site
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'jwa_site.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin_site.urls)),
+		url(r'', include('labdata.urls')),
 )
--- labdata/admin.py
+++ labdata/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from labdata.models import Person, Project
 
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from django.contrib.auth.models import User, Group
+
+class MyAdminSite(admin.AdminSite):
+	site_header = "Ajioka Lab Administration"
+
 class PersonAdmin(admin.ModelAdmin):
 	list_display = ('__str__', 'getRole')
 
-admin.site.register(Person, PersonAdmin)
-admin.site.register(Project)
+admin_site = MyAdminSite(name='myadmin')
+admin_site.register(User, UserAdmin)
+admin_site.register(Group, GroupAdmin)
+admin_site.register(Person, PersonAdmin)
+admin_site.register(Project)
 
