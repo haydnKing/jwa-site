561bbf808445eadea808fb5d24579c749a97e34f
Haydn King <hjking734@gmail.com>
2014-11-20 16:02:15 +0000

    Basic admin site

--- labdata/admin.py
+++ labdata/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from labdata.models import Person, Project
 
-# Register your models here.
+class PersonAdmin(admin.ModelAdmin):
+	list_display = ('__str__', 'getRole')
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Project)
+
--- labdata/models.py
+++ labdata/models.py
@@ -1,35 +1,51 @@
 from django.db import models
 
 class Person(models.Model):
+	class Meta:
+		verbose_name_plural = 'People'
+
+	TITLE_CHOICES = {
+		'a':'Mr',
+		'b':'Ms',
+		'c':'Mrs',
+		'd':'Miss',
+		'e':'Mx',
+		'f':'Dr',
+		'g':'Prof.',
+	}
+	ROLE_CHOICES = {
+		'a': 'Principal Investigator',
+		'b': 'Postdoctorial Researcher',
+		'c': 'Graduate Student',
+		'd': 'Undergraduate Student',
+		'e': 'Collaborator',
+		'f': 'Advisor',
+	}
+
 	name = models.CharField(max_length=512)
-	title = models.CharField(max_length=1, choices=(
-		('a','Mr'),
-		('b','Ms'),
-		('c','Mrs'),
-		('d','Miss'),
-		('e','Mx'),
-		('f','Dr'),
-		('g','Prof.'),
-		))
-	role = models.CharField(max_length=1, choices=(
-		('a','Principal Investigator'),
-		('b', 'Postdoctorial Researcher'),
-		('c', 'Graduate Student'),
-		('d', 'Undergraduate Student'),
-		('e', 'Collaborator'),
-		('f', 'Advisor'),
-		))
-	bio = models.TextField()
-	mug_shot = models.ImageField()
+	title = models.CharField(max_length=1, choices=list(TITLE_CHOICES.items()))
+	role = models.CharField(max_length=1, choices=list(ROLE_CHOICES.items()))
+	bio = models.TextField(blank=True)
+	mug_shot = models.ImageField(blank=True)
 	email = models.EmailField()
 
+	def getTitle(self):
+		return self.TITLE_CHOICES[self.title]
+	getTitle.short_description = "Title"
+	def getRole(self):
+		return self.ROLE_CHOICES[self.role]
+	getRole.short_description = "Role"
+
 	def __str__(self):
-		return dict(self.title.choices)[self.title] + self.name
+		return "{} {}".format(self.getTitle(), self.name)
 
 class Project(models.Model):
 	name = models.CharField(max_length=256)
 	short_description = models.CharField(max_length=512)
 	long_description = models.TextField()
 	person = models.ManyToManyField(Person)
+
+	def __str__(self):
+		return self.name
 
 
